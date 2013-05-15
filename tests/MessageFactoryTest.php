8dce367fb7eec1e8557e720c85d222479b9112aa
Christian Lück <christian@lueck.tv>
2013-05-15 09:30:29 +0200

    Additional ping / pong tests

--- tests/MessageFactoryTest.php
+++ tests/MessageFactoryTest.php
@@ -13,9 +13,61 @@ class MessageFactoryTest extends TestCase
 
         $message = $factory->createFromString($string);
 
+        $this->assertInstanceOf('Icmp\Message', $message);
+
         $this->assertEquals(Message::TYPE_ECHO_REQUEST, $message->getType());
         $this->assertTrue($message->isChecksumValid());
         $this->assertEquals(0, $message->getPingSequence());
         $this->assertEquals('PingHost', $message->getPayload());
+    }
+
+    public function testCreateMessagePing()
+    {
+        $factory = new MessageFactory();
+
+        $message = $factory->createMessagePing();
+
+        $this->assertInstanceOf('Icmp\Message', $message);
+
+        $this->assertEquals(Message::TYPE_ECHO_REQUEST, $message->getType());
+        $this->assertEquals(0, $message->getCode());
+        $this->assertTrue($message->isChecksumValid());
+
+        return $message;
+    }
+
+    /**
+     *
+     * @param Message $ping
+     * @depends testCreateMessagePing
+     */
+    public function testCreateMessagePong(Message $ping)
+    {
+        $factory = new MessageFactory();
+
+        $message = $factory->createMessagePong($ping);
+
+        $this->assertInstanceOf('Icmp\Message', $message);
+
+        $this->assertEquals(Message::TYPE_ECHO_REPLY, $message->getType());
+        $this->assertEquals(0, $message->getCode());
+        $this->assertEquals($ping->getPingId(), $message->getPingId());
+        $this->assertEquals($ping->getPingSequence(), $message->getPingSequence());
+        $this->assertTrue($message->isChecksumValid());
+        $this->assertEquals($ping->getPayload(), $message->getPayload());
+    }
+
+    /**
+     * assert that trying to create a pong message fails if the given message is not a ping message
+     *
+     * @expectedException InvalidArgumentException
+     */
+    public function testCreateMessagePongFailsForNonPingMessage()
+    {
+        $pingInvalid = new Message(1, 2, 3, 4);
+
+        $factory = new MessageFactory();
+
+        $factory->createMessagePong($pingInvalid);
     }
 }
