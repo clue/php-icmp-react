99bf82713981a175486afc877cf648f929950211
Christian Lück <christian@lueck.tv>
2013-05-15 09:47:57 +0200

    Test automatic generation of checksums

--- tests/MessageTest.php
+++ tests/MessageTest.php
@@ -8,13 +8,27 @@ class MessageTest extends TestCase
 {
     public function testGetters()
     {
-        $message = new Message(1, 2, null, 3, 'payload');
+        $message = new Message(1, 2, 3, 4, 'payload');
 
         $this->assertEquals(1, $message->getType());
         $this->assertEquals(2, $message->getCode());
-
-        $this->assertEquals(3, $message->getHeader());
+        $this->assertEquals(3, $message->getChecksum());
+        $this->assertEquals(4, $message->getHeader());
         $this->assertEquals('payload', $message->getPayload());
+
+
+        $this->assertFalse($message->isChecksumValid());
+    }
+
+    /**
+     * passing NULL as the checksum should automatically update the checksum to the valid, calculated value
+     */
+    public function testAutomaticMessageChecksum()
+    {
+        $message = new Message(1, 2, null, 3);
+
+        $this->assertNotNull($message->getChecksum());
+        $this->assertTrue($message->isChecksumValid());
     }
 
 
@@ -22,8 +36,6 @@ class MessageTest extends TestCase
      * asdasd
      *
      * @expectedException Exception
-     * @asd asd asd
-     * @datus-coverage ok
      */
     public function testPongInvalid()
     {
